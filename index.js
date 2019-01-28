74ad52d956364d18a0d0a94135a683bac83f54a2
SamLuther <samluther998@gmail.com>
2019-01-28 08:58:29 -0700

    pipe opened

---
+++ index.js
@@ -0,0 +1,31 @@
+var url = "https://esof-423.firebaseio.com/.json";
+var sendTo = "";
+var content = "{\"test\" : \"Oh Hey\"}";
+function update() {
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            document.getElementById("text").innerHTML = data;
+        })
+        .catch(err => {
+            console.error('An error ocurred', err);
+        })
+        
+}
+
+function sendToDB() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+             alert("Sent");
+         }
+    };
+    xhttp.open("PUT", url.slice(0, url.length-5) + sendTo + ".json", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(content);
+}
+
+update();
+//sendToDB();
