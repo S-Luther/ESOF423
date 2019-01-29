105af868d9b217ba5c6117ac29965d3d12fe84a9
Sam Luther <samluther998@gmail.com>
2019-01-29 13:29:36 -0700

    Fix on Form

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 var url = "https://esof-423.firebaseio.com/.json";
 var sendTo = "";
-var content = "{\"test2\" : \"Oh hi\"}";
+var content = "{\"userID\":\"valueascsadasd\",\"name\":\"value\",\"email\":\"value@asdasd.asd\"}";
 function update() {
     
     fetch(url)
--- public/signupvalidation.js
+++ public/signupvalidation.js
@@ -2,24 +2,22 @@ var url = "https://esof-423.firebaseio.com/.json";
 var sendTo = "";
 
 function formValidation() {
-    var id = document.registration.userId;
-    var uname = document.registration.name;
-    var uemail = document.registration.email;
+    var id = document.getElementById("uid").value;
+    var uname = document.getElementById("un").value;
+    var uemail = document.getElementById("ue").value;
     if(userid_validation(id,5,12)) {
-            if(allLetter(uname)) {
-                if(ValidateEmail(uemail)) {
-                    var content = {userID: id,name: uname,email: uemail};
-                    var myJSON = JSON.stringify(content);
-                    sendToDB(myJSON);
-                } 
-            }
+        if(allLetter(uname)) {
+            if(ValidateEmail(uemail)) {
+                var content = {userID: id,name: uname,email: uemail};
+                var myJSON = JSON.stringify(content);
+                // console.log(myJSON);
+                sendToDB(myJSON);
+            } 
         }
     }
-    return false;
-
 }
 function userid_validation(id,mx,my) {
-    var id_len = id.value.length;
+    var id_len = id.length;
     if (id_len == 0 || id_len >= my || id_len < mx) {
         alert("User Id should not be empty / length be between "+mx+" to "+my);
         id.focus();
@@ -29,7 +27,7 @@ function userid_validation(id,mx,my) {
 }
 function allLetter(uname) {
     var letters = /^[A-Za-z]+$/;
-    if(uname.value.match(letters)) {
+    if(uname.match(letters)) {
         return true;
     }
     else {
@@ -40,7 +38,7 @@ function allLetter(uname) {
 }
 function ValidateEmail(uemail) {
     var mailformat = /\S+@\S+\.\S+/;
-    if(uemail.value.match(mailformat)) {
+    if(uemail.match(mailformat)) {
         return true;
     }
     else {
@@ -49,15 +47,19 @@ function ValidateEmail(uemail) {
         return false;
     }
 }
+
 function sendToDB(content) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
+        // console.log(this.readyState);
+        // console.log(this.status);
          if (this.readyState == 4 && this.status == 200) {
              alert("Sent");
+         }else{
+
          }
     };
     xhttp.open("PUT", url.slice(0, url.length-5) + sendTo + ".json", true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send(content);
 }
-
