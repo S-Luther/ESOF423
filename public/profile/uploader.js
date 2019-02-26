e322ea35fff6d238d50266be39438b7c73c5d369
SamLuther <samluther998@gmail.com>
2019-02-25 17:24:30 -0700

    we got uploading boisss

---
+++ public/profile/uploader.js
@@ -0,0 +1,35 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDDsSMQ5fz__zVROPs58rqVnFlkcu15PF8",
+  authDomain: "esof-423.firebaseapp.com",
+  databaseURL: "https://esof-423.firebaseio.com",
+  projectId: "esof-423",
+  storageBucket: "esof-423.appspot.com",
+  messagingSenderId: "991614948455"
+};
+firebase.initializeApp(config);
+
+
+const storageService = firebase.storage();
+const storageRef = storageService.ref();
+
+document.querySelector('.file-select').addEventListener('change', handleFileUploadChange);
+document.querySelector('.file-submit').addEventListener('click', handleFileUploadSubmit);
+    
+    let selectedFile;
+function handleFileUploadChange(e) {
+  selectedFile = e.target.files[0];
+}
+
+function handleFileUploadSubmit(e) {
+  const uploadTask = storageRef.child(`images/${selectedFile.name}`).put(selectedFile); //create a child directory called images, and place the file inside this directory
+  uploadTask.on('state_changed', (snapshot) => {
+  // Observe state change events such as progress, pause, and resume
+  }, (error) => {
+    // Handle unsuccessful uploads
+    console.log(error);
+  }, () => {
+     // Do something once upload is complete
+     console.log('success');
+  });
+}
