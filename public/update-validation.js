59477a2437bce7bf33a0a7e75148c0532f04efb1
m-wintersteen <muniwintersteen@gmail.com>
2019-02-28 09:58:36 -0700

    added setRole method in update-validation.js

--- public/update-validation.js
+++ public/update-validation.js
@@ -1,7 +1,40 @@
-function updateEmail(email){
-    var userId = firebase.auth().currentUser.uid;
+var config = {
+  apiKey: "AIzaSyDDsSMQ5fz__zVROPs58rqVnFlkcu15PF8",
+  authDomain: "esof-423.firebaseapp.com",
+  databaseURL: "https://esof-423.firebaseio.com",
+  projectId: "esof-423",
+  storageBucket: "esof-423.appspot.com",
+  messagingSenderId: "991614948455"
+};
+
+firebase.initializeApp(config);
+
+document.querySelector('.role-button').addEventListener('click', setRole);
+
+function getRole(userRef){
     
+    return true;
+}
+
+function setRole(){
+    var userId = firebase.auth().currentUser.uid;
+    var database = firebase.database();
+    
+    let userRef = database.ref('users/'+userId);
+    
+    role = 'patient';
+    
+    if (document.getElementById('doctor').checked){
+        role = 'doctor'
+    }
+    userRef.update({'role':role})
+}
+
+function updateEmail(){
+    var userId = firebase.auth().currentUser.uid;
     let userRef = firebase.database().ref('users/'+userId);
+        
+    var id = document.getElementById("email").value;
     
     userRef.update({'email':email});
 }
