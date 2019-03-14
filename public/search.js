71d6e84c98604dee9e985a95c76b482a1ed72b21
m-wintersteen <muniwintersteen@gmail.com>
2019-03-14 16:27:44 -0600

    Added search.html and nav bar

---
+++ public/search.js
@@ -0,0 +1,49 @@
+var config = {
+  apiKey: "AIzaSyDDsSMQ5fz__zVROPs58rqVnFlkcu15PF8",
+  authDomain: "esof-423.firebaseapp.com",
+  databaseURL: "https://esof-423.firebaseio.com",
+  projectId: "esof-423",
+  storageBucket: "esof-423.appspot.com",
+  messagingSenderId: "991614948455"
+};
+firebase.initializeApp(config);
+
+displayAll();
+
+function displayAll(){
+    var display = document.getElementById("content");
+    var ref = firebase.database().ref().child('users');
+    
+    //Clear content
+    display.innerHTML = " ";
+    
+    // List users underneath search bar
+    ref.orderByChild("username").on("child_added", function(data) {
+        output = "";
+        
+        output += "<div class='users'>";
+        output += "<p class='username'>" + data.val().username + "</p>"
+        output += "</div>";
+        
+        display.innerHTML = display.innerHTML + output;
+    })
+}
+
+function search(){
+    var display = document.getElementById("content");
+    var ref = firebase.database().ref().child('users');
+    
+    //Clear content
+    display.innerHTML = " ";
+    
+    var input = document.getElementById("search-input").value;
+    ref.orderByChild("username").equalTo(input).on("child_added", function(data) {
+        output = "";
+        
+        output += "<div class='users'>";
+        output += "<p class='username'>" + data.val().username + "</p>"
+        output += "</div>";
+        
+        display.innerHTML = display.innerHTML + output;
+    });
+}
