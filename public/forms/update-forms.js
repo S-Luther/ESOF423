a61d5a03b5ee8a8e3fb00542ffbc6092329fee21
Andrew Dixon <acdixon91@gmail.com>
2019-03-29 16:20:17 -0600

    working to get DB updating

---
+++ public/forms/update-forms.js
@@ -0,0 +1,23 @@
+var config = {
+  apiKey: "AIzaSyDDsSMQ5fz__zVROPs58rqVnFlkcu15PF8",
+  authDomain: "esof-423.firebaseapp.com",
+  databaseURL: "https://esof-423.firebaseio.com",
+  projectId: "esof-423",
+  storageBucket: "esof-423.appspot.com",
+  messagingSenderId: "991614948455"
+};
+
+firebase.initializeApp(config);
+
+document.querySelector('.role-button').addEventListener('click', submitForm);
+
+function submitForm(){
+    var userId = firebase.auth().currentUser.uid;
+    var database = firebase.database();
+
+    let userRef = database.ref('documents/'+userId);
+
+    document.getElementById('userInfo')
+    
+    userRef.update({'formSubmitted': "yes"})
+}
