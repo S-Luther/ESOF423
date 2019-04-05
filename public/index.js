deb909401935b96a5149a2c15a15354124bb4504
m-wintersteen <muniwintersteen@gmail.com>
2019-04-04 22:56:50 -0600

    Refactoring

---
+++ public/index.js
@@ -0,0 +1,146 @@
+var UserD = "";
+function change() {
+    window.location.href = "profile/profile.html?id=" + UserD;
+}
+/**
+* Function called when clicking the Login/Logout button.
+*/
+function toggleSignIn() {
+    if (!firebase.auth().currentUser) {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/plus.login');
+        firebase.auth().signInWithRedirect(provider);
+    }
+    else {
+        firebase.auth().signOut();
+        window.location = 'index.html';
+    }
+    document.getElementById('quickstart-sign-in').disabled = true;
+}
+
+/**
+*  writes user info to the database
+*  called from within initApp funtion for when a user logs in
+*/
+
+function writeUserData(userId, name, email, imageUrl) {
+    var fireUser = firebase.auth().currentUser;
+    if(!firebase.database().ref('users/' + userId)) {
+        firebase.database().ref('users/' + userId).set({
+            username: name,
+            id: userId,
+            email: email,
+            profile_picture : imageUrl,
+            profile_type : "Patient",
+            age : "N/A",
+            gender : "N/A",
+            phone : "phoneNumber",
+            "friend_req" : {
+                "test":"data"
+            },
+            "friend_list" : {
+                "test":"data"
+            }
+        });
+    }
+    else { 
+        firebase.database().ref('users/' + userId).set({
+            username: name,
+            id: userId,
+            email: email,
+            profile_picture : imageUrl,
+            profile_type : "Patient",
+            age : "N/A",
+            gender : "N/A",
+            phone : "phoneNumber",
+            "friend_req" : {
+                "test":"data"
+            },
+            "friend_list" : {
+                "test":"data"
+            }
+        });
+    }
+    document.getElementById("theboi").disabled = false;
+
+    console.log(fireUser.createdAt)
+}
+
+/**
+* initApp handles setting up UI event listeners and registering Firebase auth listeners:
+*  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+*    out, and that is where we update the UI.
+*  - firebase.auth().getRedirectResult(): This promise completes when the user gets back from
+*    the auth redirect flow. It is where you can get the OAuth access token from the IDP.
+*/
+function initApp() {
+    firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+        var token = result.credential.accessToken;
+        document.getElementById('quickstart-oauthtoken').textContent = token;
+    }
+    else {
+        document.getElementById('quickstart-oauthtoken').textContent = 'null';
+    }
+    var user = result.user;
+
+    // calls funtion to write to firebase
+    // this might need to be called from a different location if data is being overwritten
+    writeUserData(user.uid, user.displayName, user.email, user.photoURL);
+    
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        
+        if (errorCode === 'auth/account-exists-with-different-credential') {
+            alert('You have already signed up with a different auth provider for that email.');
+            // If you are using multiple auth providers on your app you should handle linking
+            // the user's accounts here.
+        }
+        else {
+            console.error(error);
+        }
+    });
+
+    // Listening for auth state changes.
+    // [START authstatelistener]
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            UserD = user.uid;
+            // User is signed in.
+            writeUserData(user.uid, user.displayName, user.email, user.photoURL);
+
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+            document.getElementById('quickstart-account-details').textContent = displayName;
+
+        }
+        else {
+            // User is signed out.
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+            document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
+            document.getElementById('quickstart-account-details').textContent = 'null';
+            document.getElementById('quickstart-oauthtoken').textContent = 'null';
+        }
+        document.getElementById('quickstart-sign-in').disabled = false;
+    });
+
+    document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+}
+
+window.onload = function() {
+    initApp();
+};
---
+++ public/profile/profile.js
@@ -0,0 +1,182 @@
+
+var counter = 0;
+
+function displayRequests() {
+    var display = document.getElementById("recentReq");
+
+    display.innerHTML = display.innerHTML + "<h4>Friend Requests</h4>";
+
+    //var currentUser = firebase.auth().currentUser.uid;
+    var userId = "PUJRmoguPDZ18AhGZy05vpM5gJM2";
+
+    var ref = firebase.database().ref().child('users/'+userId+"/friend_req");
+
+    // Get the most recent request
+    ref.on("value", function(data) {
+        var requests = data.val();
+
+        ref.orderByChild("req").on("child_added", function(data) {
+            var reqRef = firebase.database().ref().child('users/'+data.val().req);
+
+
+            reqRef.on('value', function(snapshot) {
+                var output = "";
+                var role = "";
+
+                if(snapshot.val()!=null) {
+                    var user_name = snapshot.val().username;
+                    var role = snapshot.val().profile_type;
+                    var id = snapshot.val().id;
+
+                    output += "<div class='req'>";
+                    output += "<p>"+user_name+"</p>";
+                    output += "<p>"+role+"</p>";
+                    output += '<button type="button" onclick="addFriend(\''+ id + '\')">Accept Friend Request</button>';
+                    output += "</div>";
+                }
+                else {
+                }
+                display.innerHTML = display.innerHTML + output;
+            });
+        });
+    });
+}
+
+var id="";
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        id = user.uid;
+    }
+    else {
+        id=getUrlVars()["id"];
+    }
+});
+
+function gotofiles() {
+    window.location = 'files.html?id='+getUrlVars()["id"];
+}
+
+var url = "https://esof-423.firebaseio.com/users/"+getUrlVars()["id"]+"/";
+var fname = "username.json";
+var lname = "gender.json";
+var btype = "profile_type.json";
+var email = "email.json";
+var userId = "age.json";
+var ppic = "profile_picture.json";
+
+//Current user that is logged in
+//Change to string for developing
+//var currentUser = firebase.auth().currentUser.uid;
+var currentUser = "zSjdTDE9ZPefC9NmcvnYHzOI7U23";
+
+if(currentUser==getUrlVars()["id"]) {
+    //Viewing your Profile
+    ppic= updateInfo(url +ppic, "ppic");
+    fname= updateInfo(url + fname, "fname");
+    lname= updateInfo(url +lname, "lname");
+    btype= updateInfo(url +btype, "btype");
+    email= updateInfo(url +email, "email");
+    userId= updateInfo(url +userId, "userId");
+
+    displayRequests();
+}
+else {
+    //Viewing someone elses
+    ppic= updateInfo(url +ppic, "ppic");
+    fname= updateInfo(url + fname, "fname");
+
+    //Hide Info we don't want people seeing
+    document.getElementById("btype").style.display = "none";
+    document.getElementById("email").style.display = "none";
+    document.getElementById("userId").style.display = "none";
+    document.getElementById("lname").style.display = "none";
+    document.getElementById("nav").style.display = "none";
+
+    //Add friend button goes here
+    document.getElementById("addFriend").innerHTML = "<a href='' onclick='friendRequest()' class='button'>Add Friend</a>";
+}
+
+function friendRequest() {
+    var friendId = getUrlVars()["id"];
+    //var currentUser = firebase.auth().currentUser.uid;
+    var currentUser = "PUJRmoguPDZ18AhGZy05vpM5gJM2";
+    var userRef = firebase.database().ref().child('users/'+friendId+"/friend_req");
+
+    var newReq = userRef.push();
+
+    newReq.set ({
+        req: currentUser
+    });
+
+    alert("Friend Request Sent");
+}
+function addFriend(reqId) {
+    var currentUser = getUrlVars()["id"];
+    var userRef = firebase.database().ref().child('users/'+currentUser+"/friend_list");
+    var friendRef = firebase.database().ref().child('users/'+reqId+"/friend_list");
+    
+    var newReq = friendRef.push();
+
+    newReq.set ({
+        friend: currentUser
+    });
+
+    var newReq2 = userRef.push();
+
+    newReq2.set ({
+        friend: reqId
+    });
+    
+
+    deleteReq(reqId,currentUser);
+
+    alert("You are now friends");
+}
+
+function deleteReq(reqId,userId) {
+    var ref = firebase.database().ref().child('users/'+userId+"/friend_req");
+
+    // Get the most recent request
+    ref.on("value", function(data) {
+        var requests = data.val();
+
+        ref.orderByChild("req").equalTo(reqId).on("child_added", function(data) {
+            console.log(data.val());
+
+            ref.remove();
+        });
+    });
+
+}
+function updateInfo(value, id) {
+  console.log(value);
+
+  fetch(value)
+      .then(response => response.json())
+      .then(data => {
+          console.log(data);
+          if(data == null) {
+            document.getElementById(id).innerHTML = "";
+          }
+          else if(id == "ppic") {
+            document.getElementById(id).style = "background-image: url("+data+");";
+          }
+          else {
+            document.getElementById(id).innerHTML = document.getElementById(id).innerHTML + data;
+          }
+
+          return data;
+      })
+  .catch(err => {
+    console.error('An error ocurred', err);
+  })
+}
