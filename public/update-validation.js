32681d724f3bb2fcb209e625eac5905a368c7cd1
robotsdancing <robotsdancing@gmail.com>
2019-04-23 23:45:13 -0600

    Reformatted some JS files
    I formatted signupvalidation.js and update-validation.js  to allow for module.exports to be used so I could import functionality for tests. In theory this should change nothing with the functionality.

--- public/update-validation.js
+++ public/update-validation.js
@@ -1,104 +1,109 @@
 document.querySelector('.role-button').addEventListener('click', setRole);
-
-function updateInfo(){
+module.exports = {
+  updateInfo: function() {
     var email = document.getElementById("email").value;
     var age = document.getElementById("age").value;
     var gender = document.getElementById("gender").value;
-    
+
     var userId = firebase.auth().currentUser.uid;
-    var userRef = firebase.database().ref('users/'+userId);
-    
-    if(email!=""&&validEmail(email)){
-        updateEmail(userRef,email);
-    }
-    if(age!=""&&validAge(age)){
-        updateAge(userRef,age);
-    }
-    if(gender!=""&&validGender(gender)){
-        updateGender(userRef,gender);
-    }
-}
+    var userRef = firebase.database().ref('users/' + userId);
 
-function validGender(gender){
+    if (email != "" && validEmail(email)) {
+      updateEmail(userRef, email);
+    }
+    if (age != "" && validAge(age)) {
+      updateAge(userRef, age);
+    }
+    if (gender != "" && validGender(gender)) {
+      updateGender(userRef, gender);
+    }
+  },
+
+  validGender: function(gender) {
     //The smallest bit of data sanitation
-    if(gender.length<100){
-        return true;
+    if (gender.length < 100) {
+      return true;
+    } else {
+      alert("Quite a large gender you got there");
+      return false;
     }
-    else{
-        alert("Quite a large gender you got there");
-        return false;
-    }
-}
+  },
 
-function validAge(age){
+  validAge: function(age) {
     //Can't have a neg age
-    if(age>=0){
-        return true;
+    if (age >= 0) {
+      return true;
+    } else {
+      alert("Age cannot be less than 0")
+      return false;
     }
-    else{
-        alert("Age cannot be less than 0")
-        return false;
-    }
-}
+  },
 
-function validEmail(email) {
+  validEmail: function(email) {
     //Just making sure the email is in the form string@string.string
     var emailRegex = /\S+@\S+\.\S+/;
-    if(email==null){
-        return false;
+    if (email == null) {
+      return false;
     }
-    if(emailRegex.test(email)) {
-        return true;
+    if (emailRegex.test(email)) {
+      return true;
+    } else {
+      alert('Email Format Error');
+      return false;
     }
-    else {
-        alert('Email Format Error');
-        return false;
-    }
-}
+  },
 
-function updateEmail(userRef,email){
-    userRef.update({'email':email}); 
+  updateEmail: function(userRef, email) {
+    userRef.update({
+      'email': email
+    });
     alert("Updated Email");
     resetForm();
-}
-function updateAge(userRef,age){
-    userRef.update({'age':age}); 
+  },
+
+  updateAge: function(userRef, age) {
+    userRef.update({
+      'age': age
+    });
     alert("Updated Age");
     resetForm();
-}
-function updateGender(userRef,gender){
-    userRef.update({'gender':gender});
+  },
+  updateGender: function(userRef, gender) {
+    userRef.update({
+      'gender': gender
+    });
     alert("Updated Gender");
-    resetForm(); 
-}
-function updateRole(userRef,role){
-    userRef.update({'profile_type':role});
+    resetForm();
+  },
+  updateRole: function(userRef, role) {
+    userRef.update({
+      'profile_type': role
+    });
     alert("Updated Role");
-}
+  },
 
-function setRole(){
+  setRole: function() {
     var userId = firebase.auth().currentUser.uid;
-    var userRef = firebase.database().ref('users/'+userId);
-    
+    var userRef = firebase.database().ref('users/' + userId);
+
     role = '';
-    
-    if (document.getElementById('doctor').checked){
-        role = 'Doctor'
-        updateRole(userRef,role);
-    }
-    else if(document.getElementById('patient').checked){
-        role = 'Patient'
-        updateRole(userRef,role);
-    }
-    else{
-        alert("Please select a role");
-    }
-}
 
-function resetForm(){
+    if (document.getElementById('doctor').checked) {
+      role = 'Doctor'
+      updateRole(userRef, role);
+    } else if (document.getElementById('patient').checked) {
+      role = 'Patient'
+      updateRole(userRef, role);
+    } else {
+      alert("Please select a role");
+    }
+  },
+
+  resetForm: function() {
     document.getElementById("update-form").reset();
+  },
+  change: function() {
+    window.location.href = "profile/profile.html?id=" + localStorage.getItem("id")
+  }
 }
 
-function change() {
-    window.location.href = "profile/profile.html?id="+localStorage.getItem("id");
-}
